f68127efeac614b670ee82b5f918d72ac5dbffc0
David R. Myers <davidrmyersii@gmail.com>
2022-09-04 15:15:12 -0400

    Update tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { doTheThing } from '@voraciousdev/do-the-thing'
 import { doSomethingElse } from '@voraciousdev/do-the-thing/subpath'
 
 export const test = () => {
-  doTheThing()
-  doSomethingElse()
+  return [
+    doTheThing(),
+    doSomethingElse(),
+  ]
 }
